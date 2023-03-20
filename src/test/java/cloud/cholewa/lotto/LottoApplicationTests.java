d46ee763eaf21c46ba6cb8f0d4726390105b382a
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-03-20 20:44:33 +0100

    initial commit

---
+++ src/test/java/cloud/cholewa/lotto/LottoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
